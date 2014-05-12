3715572eddc4aea7dfa6b5d3625b5ea54c2d1244
scott2014 <626306805@qq.com>
2014-05-12 17:44:35 +0800

    gradle构建问题，正在解决中...

--- app/src/com/kitesong/Kitesong.java
+++ app/src/com/kitesong/Kitesong.java
@@ -10,9 +10,14 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
 
+import java.lang.Override;
+
 public class Kitesong extends Activity {
 	
 	private LinearLayout mEntrance = null;
+
+    //login button
+    private Button mLoginBtn = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,16 @@ public class Kitesong extends Activity {
 				startActivity(intent);
 			}
 		});
+
+        this.mLoginBtn = (Button)this.findViewById(R.id.login);
+
+        this.mLoginBtn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 	}
 
 	@Override
