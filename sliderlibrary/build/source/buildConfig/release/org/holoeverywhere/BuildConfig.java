f6aa1c9b69ba4734a455a17627c9f5a451339588
scott2014 <626306805@qq.com>
2014-05-30 14:44:47 +0800

    保存版本提交

---
+++ sliderlibrary/build/source/buildConfig/release/org/holoeverywhere/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.holoeverywhere;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "org.holoeverywhere";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2001001;
+  public static final String VERSION_NAME = "2.1.1-SNAPSHOT";
+}
